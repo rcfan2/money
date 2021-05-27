82c667a4874908b04835667abb81369bc747279d
rcfan2 <76670759+rcfan2@users.noreply.github.com>
2021-05-27 14:09:24 +0800

    Create youthshare.js

---
+++ Task/youthshare.js
@@ -0,0 +1,41 @@
+GET /client?_jdrandom=1622024601880&_funid_=task/list&functionId=task%2Flist&isNeedDealError=true&body=%7B%22modelId%22%3A%22M10007%22%2C%22plateCode%22%3A3%7D&lat=40.644543&lng=109.81208&lat_pos=40.644543&lng_pos=109.81208&city_id=805&channel=h5&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&traceId=H5_DEV_B16C6C69-7A46-4933-8406-51EBCAE4F5011622024601880&deviceToken=H5_DEV_B16C6C69-7A46-4933-8406-51EBCAE4F501&deviceId=H5_DEV_B16C6C69-7A46-4933-8406-51EBCAE4F501 h2
+Host: daojia.jd.com
+user-agent: Mozilla/5.0 (Linux; Android 10; SPN-AL00; HMSCore 5.3.0.312) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 HuaweiBrowser/11.1.0.301 Mobile Safari/537.36
+content-type: application/x-www-form-urlencoded;
+accept: */*
+sec-fetch-site: same-origin
+sec-fetch-mode: cors
+sec-fetch-dest: empty
+referer: https://daojia.jd.com/taro2orchard/h5dist/
+accept-encoding: gzip, deflate, br
+accept-language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7
+cookie: __jdc=122270672
+cookie: webp=1
+cookie: visitkey=1788543922243145
+cookie: __wga=1604996365153.1604996365153.1604996365153.1604996365153.1.1
+cookie: shshshfpa=d9a63516-6d7a-5eb4-f68a-f2783c71b2e8-1604996365
+cookie: shshshfpb=vC%2FxNLOspbWZNid67SuB5fg%3D%3D
+cookie: CCC_SE=ADC_jePQXaSwQgrMn4FMvpVbGEu8Ztx3nEKhDCByy9cP6JxnSgErXioTmAjM%2FXlbBBSH%2FfNFW%2F3hDyyJWHxs3VSFthaijy%2FSky9Bt71uqwO58vT5UvDfBYsP%2B0Ylv4ldJQkxT%2FkEvwUBbVlWk%2Bd%2BcXbwDOFmKCiDkyL3TIYA6%2BI7y5qoxWp%2BsvhrSI4z88z4kjwoh1iMODaqR1Fu3SFYrIelLIk7QmVG8fZZ7P30gKb3DkkCQAP1wZE4qFoz9ot%2FLhvLR1XtEs46OQZjR6OF0nJPu4mlCGeQaWE%2FqpdT9ZG4aJZKkZlzlffMR2cmVsW2MmLgQg6ayljQHwT7oxvfnOqt%2F6cxn13EaQmwJBcLyI7%2Fgnn1NBW3BkKtlzGxNQ4m715%2B0CW595pgciS1upjA1JXUYOjWDIqVzv3m%2FaCFTuMnedmSal7qmNWcTp%2Fc8v8F0WGhG2FFXk2LbM7CDgXxyvh545t12mOFu79N%2F6gvvlJg12f1FisWtszYYVaymMEh2MUszPbJ%2FutWDY9I8sRVaSUq%2BA%3D%3D
+cookie: CCC_Q=232945309%7Ckong%7Ct_1001802371_3225915309_5968%7Ctuiguang%7C_2_app_0_fdce70bd3b7b4429bacb82af55d5b070-p_5968
+cookie: deviceid_pdj_jd=H5_DEV_B16C6C69-7A46-4933-8406-51EBCAE4F501
+cookie: __jda=122270672.16220243966441714157350.1622024396.1622024396.1622024396.1
+cookie: __jdv=122270672%7Cdirect%7C-%7Cnone%7C-%7C1622024396648
+cookie: mba_muid=16220243966441714157350
+cookie: shshshfp=585c6a0e34e6bb7ae47aa741fa07b064
+cookie: __jdb=122270672.2.16220243966441714157350|1.1622024396
+cookie: mba_sid=16220243966578494711663585126.2
+cookie: shshshsID=2e9b9bede9dd6a955357426855cb467b_2_1622024404757
+cookie: 3AB9D23F7A4B3C9B=BHKCJ5PIVTBF4SAWVBOKZRAWVFPUXC6TRHFTPEHP53G7V2XCRN36IMK7ZW6ZDEVDFFQNJXO3VTQOS2J45AAYC3FOOU
+cookie: TrackerID=-zloq9Gd-8sJoecT9dYGlDs2zW28W9oiOYKX2sQ5KuccJy1g30G3HfJEmYdWVQ6J4COmoDf2SutXHgmTfTyR-3r2nN7NX6P2GyMpB_tJWSKAF2LOAbXVpJFEEXIm6lFqWrR06GMX29BKZZ_Prh4IbA
+cookie: pt_key=AAJgriEwADAT4CZD999f7YV8vG6ubYG2UYHrvcbykXKOkBpa-LTfyrPEgfEj3eOqybIIKrBMMhY
+cookie: pt_pin=jd_6d483637e9cd3
+cookie: pt_token=89zht9we
+cookie: pwdt_id=jd_6d483637e9cd3
+cookie: sfstoken=tk01mec1f1d1ca8sMngyKzErMSszNj6ekrSzdar5HQmuPpJhFx7OOj5X8NxPfl/1kKPn5J0q6NB4rAQx8aOi+nvyVPcr
+cookie: __jd_ref_cls=MLoginRegister_SMSLoginSuccess
+cookie: h5_coords_obj=""
+cookie: PDJ_H5_JDPIN=jd_6d483637e9cd3
+cookie: PDJ_H5_MOBILE=150****7516
+cookie: PDJ_H5_PIN=JD_8fd581160863000
+cookie: o2o_m_h5_sid=c7e870b1-2159-4060-b89b-8ceb8b6c4dc2
+
